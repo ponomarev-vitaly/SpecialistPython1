34e0e608eee9c21ba2a621df3ed8e61461dac083
ponomarev-vitaly <102874509+ponomarev-vitaly@users.noreply.github.com>
2022-04-04 13:25:17 +0300

    Update 03_task_list.py
    И еще такой вариант решения. Также получен ответ: 0.

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -2,3 +2,8 @@
 # Вывести на экран сумму всех элементов.
 
 # TODO: your code here
+list_digits = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum_digits = 0
+for digit in list_digits:
+    sum_digits += digit
+print ("Сумма всех элементов списка равна: ", sum_digits)
