f9ea9465b6f0f56655d16fffdcf189508aae6e39
ponomarev-vitaly <102874509+ponomarev-vitaly@users.noreply.github.com>
2022-04-10 01:09:03 +0300

    Update 01_hw.py
    Евгений Витальевич, направляю 3 возможных варианта. Только в последнем из 3-х не понял как убрать "," в конце последнего имени.

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -5,5 +5,9 @@ names = ["Иван", "Ирина", "Вячеслав", "Василий", "Пет
 
 # TODO: your code here
 
+print(', '.join(map(str, names)))
+print (f"{names[0]}, {names[1]}, {names[2]}, {names[3]}, {names[4]}")
+[print(name, end=", ") for name in names]
+
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
