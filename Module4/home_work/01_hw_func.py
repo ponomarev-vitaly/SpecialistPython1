4ea5f1c6cf977da1c8690e98e6b7401caea2ebfb
ponomarev-vitaly <102874509+ponomarev-vitaly@users.noreply.github.com>
2022-04-17 02:36:33 +0300

    Update 01_hw_func.py

--- Module4/home_work/01_hw_func.py
+++ Module4/home_work/01_hw_func.py
@@ -4,7 +4,19 @@
 
 def lucky_ticket(ticket_number):
     # TODO: your code here
-    pass
+    lucky_ticket = [4, 3, 6, 7, 5, 1]
+
+
+def ticket_finder():
+    summa1 = sum(lucky_ticket[:3])
+    summa2 = sum(lucky_ticket[:-3])
+    if summa1 == summa2:
+        return True
+    else:
+        return False
+
+
+print("Is your tisket lucky?", ticket_finder())
 
 
 # Тестируем функцию
